90474d8da090b3b84caf274482716678454575c0
Park-jae-hyeon <pgs1835@naver.com>
2023-12-20 17:27:13 +0900

    'redux1-Change'

--- redux1/src/Components/CounterContainer.js
+++ redux1/src/Components/CounterContainer.js
@@ -1,7 +1,7 @@
 import {useSelector, useDispatch } from "react-redux";
 // useSelector - store에 저장되어있는 값을 가져옴
 // useDispatch - 해당 액션을 실행
-import {decreaseNum, increaseNum} from "../Actions/CountAction";
+import {decreaseNum, increaseNum} from "../Actions/CountAction"; //액션 함수 불러옴
 import React from 'react';
 
 const CounterContainer = () => {
